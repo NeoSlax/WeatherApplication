56abdb0f8aafcfb3778084e91d3f12e8020e7b95
Pavel Tretyakov <mail@pstretyakov.ru>
2022-07-07 13:39:06 +0300

    - replace app icon

--- app/src/main/java/ru/neoslax/weather/ui/theme/Color.kt
+++ app/src/main/java/ru/neoslax/weather/ui/theme/Color.kt
@@ -2,9 +2,6 @@ package ru.neoslax.weather.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 val DarkBlue = Color(0xFF001064)
 val LightBlue = Color(0xFF283593)
